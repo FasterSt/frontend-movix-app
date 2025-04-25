38b5c28df8a513d96f5812323145ac70a651bffd
FasterSt <eleiker1212mendez@gmail.com>
2025-04-25 11:45:09 -0500

    add api and fetchers

--- src/api/movies/api.movies.ts
+++ src/api/movies/api.movies.ts
@@ -1,8 +1,11 @@
-import { fetcher } from "@/configs/configApi";
-import { NEXT_PUBLIC_BACKEND_URL } from "../../../config";
-import { CinemaMoviesResponse } from "@/models/types/movie";
+import { fetcher } from '@/configs/configApi';
+import { NEXT_PUBLIC_BACKEND_URL } from '../../../config';
+import { CinemaMoviesResponse } from '@/models/types/movie';
 
 export const getCinemasMovies = async (): Promise<CinemaMoviesResponse> => {
-    const { data } = await fetcher<{  data: CinemaMoviesResponse }>(`${NEXT_PUBLIC_BACKEND_URL}/api/movies`);
+    console.log('URL', `${NEXT_PUBLIC_BACKEND_URL}/api/movies`);
+    const { data } = await fetcher<{ data: CinemaMoviesResponse }>(
+        `${NEXT_PUBLIC_BACKEND_URL}/api/movies`,
+    );
     return data;
-}
+};
--- src/configs/configApi.ts
+++ src/configs/configApi.ts
@@ -1,5 +1,5 @@
-import { cookies } from "next/headers";
-import { NEXT_PUBLIC_BACKEND_URL } from "../../config";
+import { cookies } from 'next/headers';
+import { NEXT_PUBLIC_BACKEND_URL } from '../../config';
 
 const refreshToken = async () => {
     const refreshToken = cookies().get('refresh_token');
@@ -7,30 +7,36 @@ const refreshToken = async () => {
         throw new Error('No refresh token');
     }
 
-    const response = await fetch(`${NEXT_PUBLIC_BACKEND_URL}/api/auth/refresh-token`, {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
+    const response = await fetch(
+        `${NEXT_PUBLIC_BACKEND_URL}/api/auth/refresh-token`,
+        {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify({
+                refresh_token: refreshToken.value,
+            }),
         },
-        body: JSON.stringify({
-            refresh_token: refreshToken.value,
-        })
-    })
+    );
 
     if (!response.ok) {
         throw new Error('Error refreshing token');
     }
-}
+};
 
-export const fetcher = async <T>(url: string, options?: RequestInit): Promise<T> => {
+export const fetcher = async <T>(
+    url: string,
+    options?: RequestInit,
+): Promise<T> => {
     const fetchOptions: RequestInit = {
         ...options,
         credentials: 'include',
         headers: {
             'Content-Type': 'application/json',
-            'Accept': 'application/json',
+            Accept: 'application/json',
         },
     };
     const token = cookies().get('access_token');
@@ -53,5 +59,5 @@ export const fetcher = async <T>(url: string, options?: RequestInit): Promise<T>
         }
     }
 
-    return await response.json();
+    return response.json();
 };
